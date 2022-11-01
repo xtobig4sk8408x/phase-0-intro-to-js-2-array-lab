223bea283629d32d8492e0d4cd69e8fcb19e48f4
Theodore John Josef <john.josef@aol.com>
2022-11-01 08:59:05 -0700

    commit tree badafb950ada94013684d20422e05f08098a7443

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+cats.unshift(name);
+return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    const moreCats = [...cats, name];
+    return moreCats;
+}
+
+function prependCat(name) {
+    const evenMoreCats = [name, ...cats];
+    return evenMoreCats;
+}
+
+function removeLastCat() {
+    const lastCats = cats.slice (0, -1);
+    return lastCats;
+}
+
+function removeFirstCat() { 
+    const firstCats = cats.slice();
+    firstCats.shift();
+    return firstCats;
+}
